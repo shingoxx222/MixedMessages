43f86c07b3b090d1fdf6b32ce57c94c3e92d1f99
Shingo <shingoxx222@gmail.com>
2021-03-01 22:18:34 +0100

    add other two word generators

--- MixedMessages.js
+++ MixedMessages.js
@@ -3,4 +3,14 @@ function GenerateRandomDay() {
     return daysOfWeek[Math.floor(Math.random()*daysOfWeek.length)];
 }
 
-console.log(GenerateRandomDay());
+function GenerateRandomAdjective() {
+    let adjectives = ['beauitiful', 'awesome', 'sad', 'cheerful', 'fine', 'fantastic', 'grief'];
+    return adjectives[Math.floor(Math.random()*adjectives.length)];
+}
+
+function GenerateRandomWeather() {
+    let weathers = ['has sunshines', 'is cloudy', 'rains heavily', 'snows'];
+    return weathers[Math.floor(Math.random()*weathers.length)];
+}
+
+console.log(GenerateRandomDay() + " " + GenerateRandomAdjective() + " because it " + GenerateRandomWeather() + "!");
