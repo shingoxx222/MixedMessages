45334195a77a64213c05213f136f76d93bb8112c
Shingo <shingoxx222@gmail.com>
2021-03-01 22:36:13 +0100

    add sentence object

--- MixedMessages.js
+++ MixedMessages.js
@@ -13,4 +13,38 @@ function GenerateRandomWeather() {
     return weathers[Math.floor(Math.random()*weathers.length)];
 }
 
-console.log(GenerateRandomDay() + " " + GenerateRandomAdjective() + " because it " + GenerateRandomWeather() + "!");
+let sentence = {
+    _first_word:'',
+    /**
+     * @param {string} word
+     */
+    set first_word(value){
+        this._first_word = value; 
+    },
+    _second_word:'',
+    /**
+     * @param {string} word
+     */
+    set second_word(value){
+        this._second_word = value; 
+    },
+    _third_word:'',
+    /**
+     * @param {string} word
+     */
+    set third_word(value){
+        this._third_word = value; 
+    },
+    space:' ',
+    because:' because it ',
+    mark: '!',
+    fullSentence: function() {
+        return this._first_word + this.space + this._second_word + this.because + this._third_word + this.mark;
+    }
+}
+
+sentence.first_word = GenerateRandomDay();
+sentence.second_word = GenerateRandomAdjective();
+sentence.third_word = GenerateRandomWeather();
+
+console.log(sentence.fullSentence());
