5e5b5d4c8a6e14e8b322af1b680fc05b860f1dc1
Shingo <shingoxx222@gmail.com>
2021-03-01 22:12:33 +0100

    capitalize function

--- MixedMessages.js
+++ MixedMessages.js
@@ -1,6 +1,6 @@
-function generateRandomDay() {
+function GenerateRandomDay() {
     let daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
     return daysOfWeek[Math.floor(Math.random()*daysOfWeek.length)];
 }
 
-console.log(generateRandomDay());
+console.log(GenerateRandomDay());
